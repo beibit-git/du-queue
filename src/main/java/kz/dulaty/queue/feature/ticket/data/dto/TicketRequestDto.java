b1780778f73979aabb1d5d9f29d95e5dbf7cd1f4
Beibit Satov <satov_b@ezdrav.kz>
2025-08-28 17:23:27 +0500

    Add: NoArgsConstructor to TicketRequestDto

--- src/main/java/kz/dulaty/queue/feature/ticket/data/dto/TicketRequestDto.java
+++ src/main/java/kz/dulaty/queue/feature/ticket/data/dto/TicketRequestDto.java
@@ -2,10 +2,12 @@ package kz.dulaty.queue.feature.ticket.data.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TicketRequestDto {
     private String phoneNumber;
