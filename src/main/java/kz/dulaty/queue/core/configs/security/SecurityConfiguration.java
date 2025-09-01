c08d4e2538996f01236fa652cd2f506801e3a4a4
beibit_s <satov_b@ezdrav.kz>
2025-09-01 15:01:07 +0500

    Fix: security error

--- src/main/java/kz/dulaty/queue/core/configs/security/SecurityConfiguration.java
+++ src/main/java/kz/dulaty/queue/core/configs/security/SecurityConfiguration.java
@@ -65,7 +65,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
-                .headers(h -> h.frameOptions(fo -> fo.sameOrigin()))
+//                .headers(h -> h.frameOptions(fo -> fo.sameOrigin()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // Включаем CORS
                 .sessionManagement(configurer -> configurer
